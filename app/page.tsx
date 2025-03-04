b3833be3eba9224276c0d22e97f820ce1532a9e3
DESKTOP-CC44MAJ\id235791 <id235791@gmail.com>
2025-03-04 18:43:05 +0900

    section design

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,52 @@
+"use client";
 import Image from "next/image";
 import profileImage from "@/app/assets/images/profile-picture.jpg";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setAnimate(true), 500);
+  }, []);
   return (
-    <section className="pt-[4vh] h-[96vh] mx-40 box-border min-h-fit">
-      <div className="flex h-100 w-100 overflow-hidden rounded-full mx-auto">
-        <Image
-          src={profileImage}
-          alt="Dasol Jung Profile picture"
-          sizes="100"
-          className="flex justify-center gap-20 h-[80vh]"
-        ></Image>
+    <>
+      <div
+        className={`overflow-hidden ${animate ? "animate-slideToRight" : ""}`}
+      >
+        <section className={`flex justify-center gap-20 h-[80vh] relative`}>
+          <div className="flex h-100 w-100 overflow-hidden rounded-full my-auto">
+            <Image
+              src={profileImage}
+              alt="Dasol Jung Profile picture"
+              sizes="100"
+              className="flex justify-center gap-20 h-[80vh]"
+            ></Image>
+          </div>
+          <div className="self-center text-center">
+            <p className="font-semibold text-center text-3xl mb-4">
+              Hello I&apos;m
+            </p>
+            <h1 className="text-center text-5xl">Dasol Jung</h1>
+            <p className="font-semibold ">Instructor for Training Developers</p>
+            <div className="flex justify-center mt-4 gap-4">
+              <input
+                type="button"
+                className="btn btn-1"
+                value="Download CV"
+              ></input>
+              <input
+                type="button"
+                className="btn btn-2"
+                value="Download CV"
+              ></input>
+            </div>
+          </div>
+        </section>
       </div>
-      <div className="self-center text-center">
-        <p className="font-semibold text-center text-3xl mb-4">
-          Hello I&apos;m
-        </p>
-        <h1 className="text-center text-5xl">Dasol Jung</h1>
-        <p className="font-semibold ">Instructor for Training Developers</p>
-        <div className="flex justify-center mt-4 gap-4">
-          <input
-            type="button"
-            className="btn btn-1"
-            value="Download CV"
-          ></input>
-          <input
-            type="button"
-            className="btn btn-2"
-            value="Download CV"
-          ></input>
-        </div>
-      </div>
-    </section>
+      <section className="relative">
+        <p>상세소개</p>
+        <h1 className="title">About me</h1>
+      </section>
+    </>
   );
 }
